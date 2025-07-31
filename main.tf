1e9013d96f8cad97fab28e5521d1db9b16c7bd57
varundopathi <varun.reddy0077@gmail.com>
2025-07-31 10:20:03 +0530

    added new files

--- main.tf
+++ main.tf
@@ -3,7 +3,19 @@ resource "aws_security_group" "main" {
   description = var.sg_descrption
   vpc_id      = var.vpc_id
 
-  tags = {
-    Name = "allow_tls"
+  egress {
+   from_port        = 0
+   to_port          = 0
+   protocol         = "-1"
+   cidr_blocks      = ["0.0.0.0/0"]
+   ipv6_cidr_blocks = ["::/0"]
   }
+
+  tags = merge(
+    var.sg_tags,
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-${var.sg_name}"
+    }
+  )
 }
